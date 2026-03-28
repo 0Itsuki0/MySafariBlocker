18874da8be15a4412c1338fb88a8de05a72d4afe
itsuki <itsuki.enjyoy@gmail.com>
2026-03-28 15:04:43 +0900

    Initial Commit

---
+++ SafariContentBlocker/SafariContentBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariContentBlockerApp.swift
+//  SafariContentBlocker
+//
+//  Created by Itsuki on 2026/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SafariContentBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
